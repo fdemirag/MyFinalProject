140d693bd6a78fc23957e54dd88c5227f1231ff8
Fatih Demirağ <fatihdemirag@Fatihs-MacBook-Air.local>
2023-11-17 22:52:30 +0300

    rule added

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Linq.Expressions;
+using Entities.Abstract;
 using Entities.Concrete;
 
 namespace DataAccess.Abstract
 {
-	public interface IEntityRepository<T>
-	{
+	public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
         List<T> GetAll(Expression<Func<T,bool>> filter=null);
         T get(Expression<Func<T, bool>> filter);
         void Add(T entity);
