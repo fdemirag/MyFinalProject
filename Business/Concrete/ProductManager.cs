24d7ea2045381501bd43067a10905bc7347e3e27
Fatih Demirağ <fatihdemirag@Fatihs-MacBook-Air.local>
2023-11-20 03:11:37 +0300

    methods comment out

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -23,15 +23,15 @@ namespace DataAccess.Concrete
             }
 
 
-        //public List<Product> GetAllByCategoryId(int id)
-        //{
-        //    return _productDal.GetAll(p => p.CategoryId == id);
-        //}
+        public List<Product> GetAllByCategoryId(int id)
+        {
+            return _productDal.GetAll(p => p.CategoryId == id);
+        }
 
-        //public List<Product> GetByUnitPrice(decimal min, decimal max)
-        //{
-        //    return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
-        //}
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
+        }
     }
 }
 
