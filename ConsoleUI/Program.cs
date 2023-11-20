b12b03512544e036fb01c612d71e781af51fe460
Fatih Demirağ <fatihdemirag@Fatihs-MacBook-Air.local>
2023-11-20 03:13:29 +0300

    database connection success

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,7 +1,16 @@
-﻿internal class Program
+﻿using DataAccess.Concrete;
+using DataAccess.Concrete.EntityFramework;
+
+internal class Program
 {
-    private static void Main(string[] args)
+    static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        ProductManager productManager = new ProductManager(new EfProductDal());
+
+        foreach (var product in productManager.getAll())
+        {
+            Console.WriteLine(product.ProductName);
+        }
+        Console.Read();
     }
 }
---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.Intrinsics.X86;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+	public class NorthwindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost;Database=Northwind;User Id=sa;Password=reallyStrongPwd123;TrustServerCertificate=True; Encrypt=false");
+
+            //Server=localhost,1433;Database=northwind;User Id=sa;Password=12345;
+
+
+        }
+        public DbSet<Product> products { get; set; }
+
+        public DbSet<Category> categories { get; set; }
+
+        public DbSet<Customer> customers { get; set; }
+    }
+}
+
