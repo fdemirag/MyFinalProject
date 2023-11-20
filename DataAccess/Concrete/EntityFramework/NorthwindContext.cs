700dbd12c78a827b9bb16572a2b16df642d308df
Fatih Demirağ <fatihdemirag@Fatihs-MacBook-Air.local>
2023-11-20 14:44:20 +0300

    dbset added for orders

--- DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -22,6 +22,7 @@ namespace DataAccess.Concrete.EntityFramework
         public DbSet<Category> categories { get; set; }
 
         public DbSet<Customer> customers { get; set; }
+        public DbSet<Order> orders { get; set; }
     }
 }
 
