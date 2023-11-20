dbe1a6aaa3acc2793cce2c5d0092c2cab22d1606
Fatih Demirağ <fatihdemirag@Fatihs-MacBook-Air.local>
2023-11-20 03:25:52 +0300

    method added

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -6,6 +6,9 @@ namespace Business.Abstract
 	public interface IProductService
 	{
 		List<Product> getAll();
-	}
+		List<Product> GetAllByCategoryId(int id);
+		List<Product> GetByUnitPrice(decimal min, decimal max);
+
+    }
 }
 
